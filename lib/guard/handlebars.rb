6d5e0ade8905182e76073e65615cdad8d31960eb
Mark Huot <mark@markhuot.com>
2012-10-02 11:38:31 -0400

    making it a bit more flexiable and able to compile down to a single file

--- lib/guard/handlebars.rb
+++ lib/guard/handlebars.rb
@@ -6,44 +6,22 @@ module Guard
   class Handlebars < Guard
     
     def initialize(watchers = [], options = {})
-      @options = {
+      watchers = [] if !watchers
+      watchers << ::Guard::Watcher.new(%r{#{ options[:input] }/(.+\.handlebars)}) if options[:input]
+
+      options[:output] ||= options[:input]
+
+      flags = []
+      flags << 'm' if options[:min]
+      flags << 's' if options[:simple]
+      flags << 'r' if options[:root]
+      flags << 'o' if options[:knownOnly]
+      flags << 'k' if options[:known]
+      options[:flags] = flags.size > 0 ? "-#{flags.join()}" : ''
+
+      super(watchers, {
         :notifications => true
-      }.merge(options)
-      super(watchers, @options)
-    end
-    
-    def compile_handlebars file
-      content = File.new(file).read
-      begin
-        com = "handlebars #{file}"
-        result = `#{com}`
-        result
-      rescue StandardError => error
-        puts "ERROR COMPILING #{file}"
-      end
-    end
-
-    # Get the file path to output the js based on the file being
-    # built.  The output path is relative to where guard is being run.
-    #
-    # @param file [String] path to file being built
-    # @return [String] path to file where output should be written
-    #
-    def get_output(file)
-      file_dir = File.dirname(file)
-      file_name = File.basename(file).split('.')[0..-2].join('.')
-      unless file_name =~ /\.js$/
-        file_name << ".js"
-      end
-      
-      file_dir = file_dir.gsub(Regexp.new("#{@options[:input]}(\/){0,1}"), '') if @options[:input]
-      file_dir = File.join(@options[:output], file_dir) if @options[:output]
-
-      if file_dir == ''
-        file_name
-      else
-        File.join(file_dir, file_name)
-      end
+      }.merge(options))
     end
     
     def run_all
@@ -51,21 +29,47 @@ module Guard
     end
   
     def run_on_change(paths)
-      paths.each do |file|
-        output_file = get_output(file)
-        FileUtils.mkdir_p File.dirname(output_file)
-        File.open(output_file, 'w') { |f| f.write(compile_handlebars(file)) }
-        ::Guard::UI.info "# compiled handlebars in '#{file}' to js in '#{output_file}'"
-        ::Guard::Notifier.notify("# compiled handlebars in #{file}", :title => "Guard::Handlebars", :image => :success) if @options[:notifications]
+      begin
+        if @options[:compile]
+          com = "handlebars #{@options[:input]} #{@options[:flags]} -f #{@options[:output]}"
+          result = `#{com}`
+        else
+          paths.each do |file|
+            output_file = nil
+            time = Benchmark.realtime do
+              output_file = compile file
+            end
+            benchmark = "[\e[33m%2.2fs\e[0m] " % time
+            ::Guard::UI.info("\t\e[1;37mHandlebars\e[0m %s%s" % [benchmark, "#{File.basename(file)} -> #{File.basename(output_file)}"], {})
+          end
+        end
+      rescue StandardError => error
+        puts "ERROR COMPILING #{error}"
       end
-      notify paths
+
+      ::Guard::Notifier.notify("Compiled handlebars for #{paths.join(', ')}", :title => "Handlebars", :image => :success) if @options[:notifications]
     end
 
-    def notify(changed_files)
-      ::Guard.guards.reject{ |guard| guard == self }.each do |guard|
-        paths = Watcher.match_files(guard, changed_files)
-        guard.run_on_change paths unless paths.empty?
+    def compile(file)
+      output = output_path file
+      FileUtils.mkdir_p File.dirname(output)
+
+      content = File.new(file).read
+      begin
+        com = "handlebars #{file}"
+        result = `#{com}`
+      rescue StandardError => error
+        puts "Error in #{File.basename(file)}"
       end
+
+      File.open(output, 'w') { |f| f.write(result) }
+
+      output
+    end
+
+    def output_path(file)
+      file = @options[:input] ? file.sub(%r(^#{@options[:input]}/?), '') : file
+      @options[:output] + '/' + file.sub(%r(\.handlebars$), '.js')
     end
     
   end
